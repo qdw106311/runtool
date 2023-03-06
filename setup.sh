f57ad48b36f842b608354db60f34075e9fbbda44
Đỗ Quang Duy <103756160+qdw106311@users.noreply.github.com>
2023-03-06 18:03:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 home='/data/data/com.termux/files/'
 printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
-termux-setup-storage
-printf '\n\033[1;32m Tiến Hành Cài php7\n\n'
-pkg install php -y
-pkg i tur-repo && pkg i php7
-curl -s https://raw.githubusercontent.com/qdw106311/runtool/main/main -o $home/usr/bin/toaidz
+termux-setup-storage && apt update && apt upgrade && pkg i tur-repo && pkg i php7
+curl -s https://raw.githubusercontent.com/qdw106311/runtool/main/main -o $home/usr/bin/dhtool
 chmod 777 $home/usr/bin/dhtool
 
-printf '\n\033[1;92m Gõ \033[1;97mdhtool\033[1;92mĐể Vào Tool <3\n\n'
+printf '\n\033[1;32m Gõ \033[1;33mdhtool \033[1;32mĐể Vào Tool <3\n\n'
