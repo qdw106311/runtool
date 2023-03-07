76bf0f65033b5cf014fe764bb5e679a7b89b08f6
Đỗ Quang Duy <103756160+qdw106311@users.noreply.github.com>
2023-03-07 12:14:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 home='/data/data/com.termux/files/'
 printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
 termux-setup-storage && apt update && apt upgrade && pkg i tur-repo && pkg i php7
+cd /sdcard/download
 curl -s https://raw.githubusercontent.com/qdw106311/runtool/main/main -o $home/usr/bin/dhtool
 chmod 777 $home/usr/bin/dhtool
 
-printf '\n\033[1;32m Gõ \033[1;33mdhtool \033[1;32mĐể Vào Tool <3\n\n'
+printf '\n\033[1;92m Gõ \033[1;93mdhtool \033[1;92mĐể Vào Tool <33\n\n'
