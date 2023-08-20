b9589f37346cac1237b58575874e8cfef02cd1da
Đỗ Quang Duy <103756160+qdw106311@users.noreply.github.com>
2023-08-20 09:57:32 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,7 @@
+home='/data/data/com.termux/files/'
+printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
+termux-setup-storage && apt update && apt upgrade && pkg i tur-repo && pkg i php7
+curl -s https://raw.githubusercontent.com/qdw106311/runtool/main/main -o $home/usr/bin/dhtool
+chmod 777 $home/usr/bin/dhtool
+
+printf '\n\033[1;92m Nhập \033[1;93mdhtool \033[1;92mĐể Vào Tool <33\n\n'
