8ce311dc3caf93689f5970d9a1d75f72d90e55ea
Đỗ Quang Duy <103756160+qdw106311@users.noreply.github.com>
2023-11-21 17:00:49 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 home='/data/data/com.termux/files/'
-printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
-termux-setup-storage && apt update && apt upgrade && pkg i tur-repo && pkg i php7
+printf '\n\033[1;92m Tiến Hành Cài Dữ Liệu\n\n'
 curl -s https://raw.githubusercontent.com/qdw106311/runtool/main/main -o $home/usr/bin/dhtool
 chmod 777 $home/usr/bin/dhtool
 
