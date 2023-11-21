443f4ed5fd8949cb0a64e94b243587d509415a2f
Đỗ Quang Duy <103756160+qdw106311@users.noreply.github.com>
2023-11-21 17:08:02 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 home='/data/data/com.termux/files/'
 printf '\n\033[1;92m Tiến Hành Cài Dữ Liệu\n\n'
-curl -s https://raw.githubusercontent.com/qdw106311/runtool/main/main -o $home/usr/bin/dhtool
+curl -s https://raw.githubusercontent.com/qdw106311/runtool/main/dh.php -o $home/usr/bin/dhtool
 chmod 777 $home/usr/bin/dhtool
 
 printf '\n\033[1;92m Nhập \033[1;93mdhtool \033[1;92mĐể Vào Tool <33\n\n'
